7e01fd44bca9bb3ac4b82425bf566fd3082add06
SunamCahai <94171875+SunamCahai@users.noreply.github.com>
2026-02-17 20:55:55 +0000

    Added opacity func and cleaning button

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,51 @@
 function changeColor(e) {
     e.target.style.backgroundColor = 'black';
 }
-function createGrid() {
-    const container = document.querySelector('.container');
-    container.innerHTML = '';
-    for (let i = 0; i<16; i++) {
-        for (let j = 0; j<16; j++) {
-            const newDiv = document.createElement("div");
-            newDiv.classList.add("square");
-            newDiv.addEventListener('mouseenter', changeColor);
-            container.appendChild(newDiv);
-        }
+
+
+function changeOpacity(e) {
+    let currentOpacity = parseFloat(e.target.style.opacity) || 0;
+    if (currentOpacity < 1) {
+        e.target.style.opacity = currentOpacity + 0.1;
     }
 }
-createGrid();
-document.addEventListener("DOMContentLoaded", createGrid);
+
+function createGrid(size, a) {
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+
+    container.style.display = 'grid';
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i<size*size; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("square");
+        newDiv.style.opacity = "0";
+        newDiv.addEventListener('mouseenter', changeColor);
+        newDiv.addEventListener('mouseenter', changeOpacity);
+        container.appendChild(newDiv);
+    }
+
+}
+
+const changeGrid = document.querySelectorAll('.btn');
+changeGrid.forEach(button => {
+    button.addEventListener('click', () => {
+        const size = prompt('Enter number between 1-100?');
+        if (size > 1 && size < 100) {
+            createGrid(size);
+        } else {
+            alert("wrong number, try again");
+        }
+    })
+});
+
+const cleanGrid  = document.querySelector('.cleanbtn');
+cleanGrid.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.backgroundColor = 'white';
+        square.style.opacity = '0';
+    });
+});
